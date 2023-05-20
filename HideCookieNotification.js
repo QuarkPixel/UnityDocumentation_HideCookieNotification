51b88b46eaa56f30b87f14a39fc2206b22ab61ca
QuarkPixel <m2462864403@gmail.com>
2023-05-20 19:09:16 +0800

    add some information | fixed the bug

--- HideCookieNotification.js
+++ HideCookieNotification.js
@@ -1,16 +1,18 @@
 // ==UserScript==
 // @name        HideCookieNotification
+// @namespace   https://github.com/QuarkPixel/UnityDocumentation_HideCookieNotification
 // @description Hide cookie notification in local documentation.
 // @match       file:///*/Editor/Data/Documentation/*.html*
-// @version     0
+// @version     0.0.2
+// @author     QuarkPixel
 // @grant       none
 // ==/UserScript==
 
-;(function () {
-	'use strict'
-	let css = `
-    #onetrust-consent-sdk {display: none;
-}    
-    `
-	GM_addStyle(css)
-})
+{
+	let style = document.createElement('style')
+	style.type = 'text/css'
+	let text = document.createTextNode('#onetrust-consent-sdk{display: none;}')
+	style.appendChild(text)
+	let head = document.getElementsByTagName('head')[0]
+	head.appendChild(style)
+}
